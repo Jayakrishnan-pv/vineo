d805da90055ce82c027222cc4dbc6e072528d8df
Jayakrishnanpv <85985350+Jayakrishnanpv@users.noreply.github.com>
2024-10-15 15:41:10 +0530

    added pagination feature

--- src/app/[locale]/(auth)/(center)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/[locale]/(auth)/(center)/sign-in/[[...sign-in]]/page.tsx
@@ -5,12 +5,13 @@ import Image from 'next/image';
 import React from 'react';
 
 import LoginForm from '@/components/LoginForm';
+import { IMAGES } from '@/constants/page';
 
 const LoginPage: React.FC = () => {
   return (
     <div className="flex size-full h-screen items-center justify-around bg-glass-bottle bg-[length:800px_550px] bg-left-bottom bg-no-repeat ">
       <div className="h-screen sm:w-0 md:w-0 lg:w-1/2">
-        <Image src="/assets/images/vineo-logo.png" alt="vineo-logo" className="absolute z-0 my-10 ml-5" width={130} height={120} />
+        <Image src={IMAGES.vineoLogo} alt="vineo-logo" className="absolute z-0 my-10 ml-5" width={130} height={120} />
       </div>
       <div className="mr-10 rounded-2xl bg-white/80 p-20 shadow-[0px_0px_13px_1px_rgba(0,0,0,0.15)] lg:w-1/3">
         <div className="mb-16 text-center">
---
+++ src/constants/page.tsx
@@ -0,0 +1,12 @@
+export const IMAGES = {
+  vineoLogo: '/assets/images/vineo-logo.png',
+  homeBg: '/assets/images/homebg.png',
+  graph: '/assets/images/graph.svg',
+  coin: '/assets/images/logo/coin.svg',
+  vector: '/assets/images/logo/vector.svg',
+  settings: '/assets/images/logo/settings.svg',
+  star: '/assets/images/logo/star.svg',
+  inbox: '/assets/images/logo/inbox-Regular.svg',
+  home: '/assets/images/logo/home.svg',
+  google: '/assets/images/logo/flat-color-icons_google.png',
+};
